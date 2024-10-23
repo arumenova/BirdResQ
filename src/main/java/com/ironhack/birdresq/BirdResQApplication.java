fed1880f66e5358552d28f0ba9f06b9f3aad0d9a
Adriana Yordanova <arumenova4@gmail.com>
2024-10-23 15:18:51 +0200

    created structure of the project

---
+++ src/main/java/com/ironhack/birdresq/BirdResQApplication.java
@@ -0,0 +1,13 @@
+package com.ironhack.birdresq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BirdResQApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BirdResQApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ironhack/birdresq/BirdResQApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ironhack.birdresq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BirdResQApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
